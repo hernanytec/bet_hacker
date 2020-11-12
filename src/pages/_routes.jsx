1c9f35a21b6785db3712dee2fd4c5f329dd2dbbb
Rafael Lima <rglllima@gmail.com>
2020-11-11 23:36:06 -0300

    Solved Conflicts

--- src/pages/_routes.jsx
+++ src/pages/_routes.jsx
@@ -6,30 +6,20 @@ import ProjectsPage from "./projects";
 import BookmakersPage from "./Bookmakers";
 import SportsPage from "./Sports";
 import ZoomPage from "./Zoom";
-<<<<<<< HEAD
-import HomePage from "./Home";
 import SignIn from "./SignIn";
-=======
 import OpportunitiesPage from "./Opportunities";
 import PercentPage from "./Percent";
-
->>>>>>> 920956127ee5fc3d5862f8ebf62b61b06a2384c5
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-<<<<<<< HEAD
         <Route path="/login">
           <SignIn />
         </Route>
 
-        <Route path="/members">
-          <MembersPage />
-=======
         <Route path="/Bookmakers">
           <BookmakersPage />
->>>>>>> 920956127ee5fc3d5862f8ebf62b61b06a2384c5
         </Route>
         
         <Route path="/projects">
