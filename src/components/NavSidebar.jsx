49ff5714dc545339e9f4b4483c7c610cde296d90
hernany <hernanytec777@Gmail.com>
2020-11-17 15:12:15 -0300

    add base of side menu bar

--- src/components/NavSidebar.jsx
+++ src/components/NavSidebar.jsx
@@ -1,75 +1,73 @@
-import { Navigation } from "react-minimal-side-navigation"; 
-import { useHistory, useLocation } from "react-router-dom";
 import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { DiAptana } from "react-icons/di";
 import { FaSearchPlus } from "react-icons/fa";
 import { HiOutlineSwitchVertical } from "react-icons/hi";
 import { AiOutlinePercentage, AiOutlineUnorderedList } from "react-icons/ai";
-import { BiBasketball} from "react-icons/bi";
+import { BiBasketball } from "react-icons/bi";
 import BetHackerIcon from '../BetHackerIcon.jpeg'
-
-
-import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
 
 export const NavSidebar = () => {
   const history = useHistory();
   const location = useLocation();
-  
+
+  const pageSelected = location.pathname
+
+  const items = [
+    {
+      render: () => <FaSearchPlus />,
+      id: "/zoom",
+    },
+    {
+      render: () => <HiOutlineSwitchVertical />,
+      id: "/projects",
+    },
+    {
+      render: () => <AiOutlineUnorderedList />,
+      id: "/bookmakers",
+    },
+    {
+      render: () => <AiOutlinePercentage />,
+      id: "/percent",
+    },
+    {
+      render: () => <BiBasketball />,
+      id: "/sports",
+    },
+  ]
+
+  const changeSelected = (pageId) => {
+    history.push(pageId)
+  }
+
+  const getClasses = (pageId) =>{
+    if(pageSelected === pageId){
+      return 'nav-sidebar__item--selected'
+    }
+  }
 
   return (
-    <React.Fragment>
-      <div
-        className={`fixed inset-y-0 left-0 z-30 w-24 transition duration-300 ease-out transform translate-x-0 bg-black border-r-2 lg:translate-x-0 lg:static lg:inset-0 "
-        }`}
-      >
-        <Navigation
-          activeItemId={location.pathname}
-          onSelect={({ itemId }) => {
-            history.push(itemId);
-          }}
-          items={[
-            {
-              elemBefore: () => <img src={BetHackerIcon} alt="bet hacker icon" />,
-              itemId: "/Opportunities",
-            },
-            {
-              elemBefore: () => <FaSearchPlus style={{ fontSize: 25 }}/>,
-              itemId: "/zoom",
-            },
-            {
-              elemBefore: () => <HiOutlineSwitchVertical style={{ fontSize: 30 }} />,
-              itemId: "/projects",
-            },
-            {
-              elemBefore: () => <AiOutlineUnorderedList style={{ fontSize: 30 }}/>,
-              itemId: "/bookmakers",
-            },
-            {
-              elemBefore: () => <AiOutlinePercentage style={{ fontSize: 30 }}/>,
-              itemId: "/percent",
-            },
-            {
-              elemBefore: () => <BiBasketball style={{ fontSize: 30 }}/>,
-              itemId: "/sports",
-            },
-          ]}
-        />
+    <div className="nav-sidbar">
+      <div className="nav-header">
+        <img src={BetHackerIcon} alt="bet hacker icon" />
+      </div>
 
-        <div className="absolute bottom-0 w-full my-8">
-          <Navigation
-            activeItemId={location.pathname}
-            items={[
-              {
-                itemId: "/Setings",
-                elemBefore: () => <DiAptana style={{ fontSize: 30 }}/>,
-              },
-            ]}
-            onSelect={({ itemId }) => {
-              history.push(itemId);
-            }}
-          />
+      <div className="nav-filters">
+
+        {
+          items.map(item => (
+            <div onClick={() => changeSelected(item.id)} className={`nav-sidebar__item ${getClasses(item.id)}`}>
+              { item.render()}
+            </div>
+          ))
+        }
+      </div>
+
+      <div onClick={() => history.push('/setings')} className="nav-config">
+        <div className={`nav-sidebar__item ${getClasses('/setings')}`}>
+          <DiAptana />
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
